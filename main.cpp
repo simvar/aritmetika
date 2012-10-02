2efef976a19e84646ba73d03ee667ec026fa9706
simvar <simavark@gmail.com>
2012-10-02 16:43:42 +0200

    Realizavau sudeties operacija

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
+int sudetis(int x,int y){
+return x+y;
+}
 int main (int argc, char const *argv[])
 {
-	cout << "Sveika aritmetika!";
+	int x=10; int y=5;
+	cout << "Sveika aritmetika!"<<endl;
+	cout << "x+y= "<<sudetis(x,y)<<endl;
+
 	return 0;
 }
