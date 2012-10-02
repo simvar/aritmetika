b031053e4038d636fe98d3bd750f49096696d89d
simvar <simavark@gmail.com>
2012-10-02 22:58:15 +0300

    Prideta mod

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
+int mod(int x,int y){
+return x%y;
 }
 int main (int argc, char const *argv[])
 {
-	cout << "Sveika aritmetika!"<<endl;
+	cout <<x<< " mod "<<y<<" = "<<mod(x,y)<<endl;
 	return 0;
 }
