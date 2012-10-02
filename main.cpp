148cf7ac7ed7e38496d3c9736b2216ca7a71a54c
simvar <simavark@gmail.com>
2012-10-02 23:00:29 +0300

    Prideta div

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
+int divv(int x,int y){
+return x/y;
 }
 int main (int argc, char const *argv[])
 {
-	cout << "Sveika aritmetika!"<<endl;
+	cout <<x<<" + "<<y<<" = "<<divv(x,y)<<endl;
 	return 0;
 }
