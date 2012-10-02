e9ad1f854ca339b12979e8d707f9c298d82ada82
simvar <simavark@gmail.com>
2012-10-02 22:47:53 +0300

    Prideta daugyba

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
+int daugyba(int x, int y){
+return x*y;
 }
 int main (int argc, char const *argv[])
 {
-	cout << "Sveika aritmetika!"<<endl;
+        cout <<x<< " * "<<y<<" = "<<daugyba(x,y)<<endl;
 	return 0;
 }
