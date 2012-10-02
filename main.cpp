9d7cf790beb5deb323ebcdca4db796d3ef595106
simvar <simavark@gmail.com>
2012-10-02 23:41:37 +0300

    Redaguota sudetis

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
-int sudetis(int x,int y){
-return x+y;
-}
+int sudetis(int x,int y){return x+y;}
 int main (int argc, char const *argv[])
 {
 	cout << x<<" + "<<y<<" = "<<sudetis(x,y)<<endl;
